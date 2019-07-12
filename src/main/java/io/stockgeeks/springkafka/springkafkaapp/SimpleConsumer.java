78a040dda2d348ee201c55a5be1927b51d63e709
Marcos Maia <maia.marcos@gmail.com>
2019-07-13 00:56:43 +0200

    small adjustment and using lombok

--- src/main/java/io/stockgeeks/springkafka/springkafkaapp/SimpleConsumer.java
+++ src/main/java/io/stockgeeks/springkafka/springkafkaapp/SimpleConsumer.java
@@ -1,12 +1,14 @@
 package io.stockgeeks.springkafka.springkafkaapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class SimpleConsumer {
   @KafkaListener(id = "simple-consumer", topics = "simple-message")
   public void consumeMessage(String message) {
-    System.out.println("Got message: " + message);
+    log.info("Consumer got message: {}", message);
   }
 }
